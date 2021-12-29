feef35bdf4f2a11fde8a035aecf2da5801e5d7a4
MatCoder77 <matkimzag@wp.pl>
2021-12-29 22:17:33 +0100

    [BUILD] Add keycloak and PostgreSQL to docker-compose

--- user-service/user-service-core/src/main/java/com/flatrental/userservice/TestController.java
+++ user-service/user-service-core/src/main/java/com/flatrental/userservice/TestController.java
@@ -3,9 +3,11 @@ package com.flatrental.userservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/test")
+@RestController
+@RequestMapping("/api/test")
 @RefreshScope
 public class TestController {
 
